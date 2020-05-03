66dc3176d0c188cf13a4f4f642e912f79f0f8481
Daniel Reyes <daniel.c.reyes30@gmail.com>
2020-05-03 13:14:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,9 @@
 require "yaml"
 
 def load_library(file_path)
-  emoticons = YAML.load_file(file_path)
-  new_hash = {}
-  emoticons.each do |key,value|
+  emoticons = YAML.load_file(file_path) # emoticons is the vriable for the yaml file of emoticons in both english and japanese
+  new_hash = {} # new hash represented by empty curly brackets, return new has at the end
+  emoticons.each do |key,value| # emoticons are looped thru via .each method, gone thru both key and value
       new_hash[key] = {}
       new_hash[key][:english] = value[0]
       new_hash[key][:japanese] = value[1]
@@ -14,6 +14,13 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon
+  library = load_library(file_path)
+  emoticon = library.keys.find do |key|
+    library[key][:english] == emoticon
+    emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
+  end
+
+  
   # code goes here
 end
 
